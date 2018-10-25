3dc13fd29f97c64af8c951e393f2f86768012354
Alta1r <kolya.tanchinec@mail.ru>
2018-10-26 02:08:10 +0300

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace bank_openedu_test
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            string UserInput = Console.ReadLine();
+
+            Console.WriteLine(Calculate(UserInput));
+            Console.ReadLine();
+        }
+        public static double Calculate(string UserInput)
+        {
+            var input_split = UserInput.Split(' ');
+            double startProc = Convert.ToDouble(input_split[1]);
+            double money = Convert.ToDouble(input_split[0]);            
+            double proc = Convert.ToDouble(input_split[1]);
+            proc = proc / 12;             
+            double period = double.Parse(input_split[2]);
+
+
+            if (period > 0)
+            {
+                money = money + ((money / 100) * proc);
+                period -= 1;
+                return Calculate(Convert.ToString(money) + " " + startProc + " " + Convert.ToString((period)));
+                
+
+            }
+            else  return money; 
+
+
+        }
+
+
+    }
+}
+
